95ad0a26c796f1db11b132d94fb867e0ba1c164d
james <uchennaanih16@gmail.com>
2022-07-23 08:37:29 +0100

    added express response handler

--- src/modules/misc/misc.controller.ts
+++ src/modules/misc/misc.controller.ts
@@ -1,12 +1,16 @@
 import { MiscService } from './misc.service';
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Res } from '@nestjs/common';
+import { Response } from 'express';
 
 @Controller('/api')
 export class MiscController {
   constructor(private readonly miscService: MiscService) {}
 
   @Get('/all/banks')
-  async getbanks() {
-    return await this.miscService.banklist();
+  async getbanks(@Res() res: Response) {
+    const result = await this.miscService.banklist();
+    if (result.error) return res.status(400).json(result);
+
+    return res.status(200).json(result);
   }
 }

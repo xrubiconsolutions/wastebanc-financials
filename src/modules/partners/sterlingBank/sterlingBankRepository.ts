86d026022a29a0e74c8238debd5b38121fbfad9a
james <uchennaanih16@gmail.com>
2022-08-12 15:52:54 +0100

    fix

--- src/modules/partners/sterlingBank/sterlingBankRepository.ts
+++ src/modules/partners/sterlingBank/sterlingBankRepository.ts
@@ -1,78 +1,100 @@
 import axios from 'axios';
 
-export const makeRequest = async (requestObj: any) => {
+export const makeRequest = async (requestObj: any, contentType: string) => {
   requestObj.headers = {
     Channel: process.env.STERLINGCHANNEL,
     Authorization: `${process.env.STERLINGCHANNEL} ${process.env.STERLINGNAME} ${process.env.STERLINGKEY}`,
-    'Content-Type': 'text/plain; charset=utf-8',
+    'Content-Type': contentType,
   };
   requestObj.url = `${process.env.STERLING_URL + requestObj.url}`;
+  console.log('header', requestObj.headers);
   const result = await axios(requestObj);
   return result.data;
 };
 
 export const getBankList = async () => {
-  const bankList = await makeRequest({
-    method: 'get',
-    url: 'Transaction/BankList',
-  });
+  const bankList = await makeRequest(
+    {
+      method: 'get',
+      url: 'Transaction/BankList',
+    },
+    'text/plain; charset=utf-8',
+  );
   console.log('sterling result', bankList);
   return bankList;
 };
 
 export const nipAccountNumber = async (accountData: any) => {
-  const account = await makeRequest({
-    method: 'post',
-    url: 'Transaction/NIPNameinquiry',
-    value: accountData,
-  });
+  const account = await makeRequest(
+    {
+      method: 'post',
+      url: 'Transaction/NIPNameinquiry',
+      value: accountData,
+    },
+    'application/json',
+  );
   return account;
 };
 
 export const getCustomerInformation = async (accountData: any) => {
-  const account = await makeRequest({
-    method: 'get',
-    url: `Transaction/customerInformation/${accountData}`,
-  });
+  const account = await makeRequest(
+    {
+      method: 'get',
+      url: `Transaction/customerInformation/${accountData}`,
+    },
+    'application/json',
+  );
   return account;
 };
 
 export const nipFundTransfer = async (transferData: any) => {
-  const response = await makeRequest({
-    method: 'post',
-    url: 'Transaction/NIPFundTransfer',
-    value: transferData,
-  });
+  const response = await makeRequest(
+    {
+      method: 'post',
+      url: 'Transaction/NIPFundTransfer',
+      value: transferData,
+    },
+    'application/json',
+  );
 
   return response;
 };
 
 export const intraBank = async (transferData: any) => {
-  const response = await makeRequest({
-    method: 'post',
-    url: 'Transaction/IntraBank',
-    value: transferData,
-  });
+  const response = await makeRequest(
+    {
+      method: 'post',
+      url: 'Transaction/IntraBank',
+      value: transferData,
+    },
+    'application/json',
+  );
 
   return response;
 };
 
 export const generateVirtualAccount = async (virtualAccountData: any) => {
-  const virtualAccount = await makeRequest({
-    method: 'post',
-    url: 'CreateAccount/OpenAccount',
-    value: virtualAccountData,
-  });
+  const virtualAccount = await makeRequest(
+    {
+      method: 'post',
+      url: 'CreateAccount/OpenAccount',
+      value: virtualAccountData,
+    },
+    'application/json',
+  );
 
   return virtualAccount;
 };
 
 export const verifyTransfer = async (transferData: any) => {
-  const response = await makeRequest({
-    method: 'post',
-    url: '/api/Transaction/VerifyTransaction',
-    value: transferData,
-  });
+  const response = await makeRequest(
+    {
+      method: 'post',
+      url: '/api/Transaction/VerifyTransaction',
+      value: transferData,
+    },
+    'application/json',
+  );
 
   return response;
 };

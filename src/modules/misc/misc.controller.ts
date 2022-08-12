9b549186011b1cd9b2fbb0f7fd38f61e4250308c
james <uchennaanih16@gmail.com>
2022-08-12 22:46:27 +0100

    fix

--- src/modules/misc/misc.controller.ts
+++ src/modules/misc/misc.controller.ts
@@ -23,11 +23,11 @@ export class MiscController {
     @Query('bank_code') bank_code: string,
     @Res() res: Response,
   ) {
-    const ref = randomInt(1000);
+    const ref = randomInt(1000000);
     const params: resolveAccountDTO = {
       accountNumber: account_number,
       BankCode: bank_code,
-      referenceId: ref.toString(),
+      referenceId: '13435',
     };
 
     const result = await this.miscService.resolveAccountNumber(params);

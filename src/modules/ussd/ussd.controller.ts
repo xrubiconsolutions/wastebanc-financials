2596ac61219f69ec2bf23d994eeebc6158c7882a
james <uchennaanih16@gmail.com>
2022-12-09 10:29:23 +0100

    changed ussd initate to post

--- src/modules/ussd/ussd.controller.ts
+++ src/modules/ussd/ussd.controller.ts
@@ -1,12 +1,12 @@
 import { ussdService } from './ussd.service';
-import { Controller, Get, Res } from '@nestjs/common';
+import { Controller, Get, Post, Res } from '@nestjs/common';
 import { Response } from 'express';
 
 @Controller('/connect/ussd')
 export class ussdController {
   constructor(private readonly UssdService: ussdService) {}
 
-  @Get('/initiate')
+  @Post('/initiate')
   async initateUssd(@Res() res: Response) {
     const result = await this.UssdService.initate();
     return res.status(200).json(result);

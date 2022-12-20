c0612715e45055e13f44b1380e896da76a9358b6
james <uchennaanih16@gmail.com>
2022-12-20 15:59:03 +0100

    more updates

--- src/modules/ussd/ussd.controller.ts
+++ src/modules/ussd/ussd.controller.ts
@@ -1,13 +1,15 @@
 import { ussdService } from './ussd.service';
-import { Controller, Get, Post, Res } from '@nestjs/common';
-import { Response } from 'express';
+import { Controller, Get, Post, Res, Req } from '@nestjs/common';
+import { Response, Request } from 'express';
 
 @Controller('/connect/ussd')
 export class ussdController {
   constructor(private readonly UssdService: ussdService) {}
 
   @Post('/initiate')
-  async initateUssd(@Res() res: Response) {
+  async initateUssd(@Res() res: Response, @Req() req: Request) {
+    const body = req.body;
+    console.log('body', body);
     const result = await this.UssdService.initate();
     return res.status(200).json(result);
   }
